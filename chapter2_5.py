c86681640004b818e85305dce1caeaea9a6c3572
sagapref0001 <sagapref0001@gmail.com>
2023-12-18 18:06:40 +0900

    python2年生

--- chapter2_5.py
+++ chapter2_5.py
@@ -1,9 +1,10 @@
 import PySimpleGUI as sg
+sg.theme("BrightColors")
 
 layout = [[sg.Input("フタバ", key="in")],
           [sg.Button("実行", key="btn")],
           [sg.Text(key="txt")]]
-window = sg.Window("あいさつテスト",layout)
+window = sg.Window("あいさつテスト",layout,font=(None,14),size=(250,120))
 
 def execute():
     txt = "こんにちは、"+ values["in"] + "さん！"
